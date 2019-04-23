b0f2289614379eeff06a06e0dace67200719ae2e
botikmot <49896286+gonzrock@users.noreply.github.com>
2019-04-23 14:30:32 +0800

    Add files via upload
    initial commit

---
+++ index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE HTML>
+<!--
+	Read Only by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Botikmot</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<section id="header">
+				<header>
+					<span class="image avatar"><img src="images/rock.jpg" alt="" /></span>
+					<h1 id="logo"><a href="#">Rock Gonz</a></h1>
+					<p>IT Specialist // Web Developer</p>
+				</header>
+				<nav id="nav">
+					<ul>
+						<li><a href="#one" class="active">About</a></li>
+						<li><a href="#two">Things I Can Do</a></li>
+						<li><a href="#three">A Few Accomplishments</a></li>
+						<li><a href="#four">Contact</a></li>
+					</ul>
+				</nav>
+				<footer>
+					<ul class="icons">
+						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>
+						<li><a href="#" class="icon fa-envelope"><span class="label">Email</span></a></li>
+					</ul>
+				</footer>
+			</section>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<div id="main">
+
+						<!-- One -->
+							<section id="one">
+								<div class="image main" data-position="center">
+									<img src="images/banner2.jpg" alt="" />
+								</div>
+								<div class="container">
+									<header class="major">
+										<h3>About Me</h3>
+										<!--<p>Secure a position in a creative and dynamic work environment where I can apply my skills and to build and maintain a websites to fit the needs to the company or customer.</p>-->
+									</header>
+									<!--<p>Faucibus sed lobortis aliquam lorem blandit. Lorem eu nunc metus col. Commodo id in arcu ante lorem ipsum sed accumsan erat praesent faucibus commodo ac mi lacus. Adipiscing mi ac commodo. Vis aliquet tortor ultricies non ante erat nunc integer eu ante ornare amet commetus vestibulum blandit integer in curae ac faucibus integer non. Adipiscing cubilia elementum.</p>-->
+									<p>
+											<span>Hello, My name is Rocky Gonzales </span> I'm IT Specialist living in Philippines and I make Web Applications, usually with PHP/Laravel, and ready for new opportunities. I love music, watching movies, and playing basketball.
+										</p>
+										<p>
+											I have experience with HTML, CSS, Boostrap, Javascript, Jquery, Github and Photoshop as well.
+										</p>
+								</div>
+							</section>
+
+						<!-- Two -->
+							<section id="two">
+								<div class="container">
+									<h3>Things I Can Do</h3>
+									<p>These are the things I can do as an IT Specialist/Web Developer:</p>
+									<ul class="feature-icons">
+										<li class="fa-code">Write codes</li>
+										<li class="fa-question">Solve a Problem with Google</li>
+										<li class="fa-book">Read ebooks and tutorials</li>
+										<li class="fa-desktop">PC Troubleshooting</li>
+									</ul>
+								</div>
+							</section>
+
+						<!-- Three -->
+							<section id="three">
+								<div class="container">
+									<h3>A Few Accomplishments</h3>
+									<p>I am so grateful that this government institution gave me this opportunity to develop this kind of projects. This is also not for them but also for me to enhanced my capability and skills.</p>
+									<div class="features">
+										<article>
+											<a href="#" class="image"><img src="images/edats.jpg" alt="" /></a>
+											<div class="inner">
+												<h4>Document Action Tracking System</h4>
+												<p>A Web based Document Action Tracking System for DENR Caraga. To Track Incoming/Outgoing Documents and to identify Users for the late Actions. <span style="font-size: 12px;"> (PHP Laravel Framework, Mysql Database)</span></p> 
+											</div>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/pic02.jpg" alt="" /></a>
+											<div class="inner">
+												<h4>ICT inventory</h4>
+												<p>A local Web based ICT Inventory System. This system can identify obsolete parts/units of computers. <span style="font-size: 12px;"> (PHP HTML CSS Javascript, Mysql Database)</span></p>
+											</div>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/pic03.jpg" alt="" /></a>
+											<div class="inner">
+												<h4>Online Submission System</h4>
+												<p>My Recent Project in DENR Caraga.</p>
+											</div>
+										</article>
+									</div>
+								</div>
+							</section>
+
+						<!-- Four -->
+							<section id="four">
+								<div class="container">
+									<h3>Contact Me</h3>
+									<p>Integer eu ante ornare amet commetus vestibulum blandit integer in curae ac faucibus integer non. Adipiscing cubilia elementum integer. Integer eu ante ornare amet commetus.</p>
+									<form method="post" action="#">
+										<div class="row gtr-uniform">
+											<div class="col-6 col-12-xsmall"><input type="text" name="name" id="name" placeholder="Name" /></div>
+											<div class="col-6 col-12-xsmall"><input type="email" name="email" id="email" placeholder="Email" /></div>
+											<div class="col-12"><input type="text" name="subject" id="subject" placeholder="Subject" /></div>
+											<div class="col-12"><textarea name="message" id="message" placeholder="Message" rows="6"></textarea></div>
+											<div class="col-12">
+												<ul class="actions">
+													<li><input type="submit" class="primary" value="Send Message" /></li>
+													<li><input type="reset" value="Reset Form" /></li>
+												</ul>
+											</div>
+										</div>
+									</form>
+								</div>
+							</section>
+
+
+					</div>
+
+				<!-- Footer -->
+					<section id="footer">
+						<div class="container">
+							<ul class="copyright">
+								<li>&copy; 2019 All rights reserved.</li><li><a href="#">Botikmot</a></li>
+							</ul>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
